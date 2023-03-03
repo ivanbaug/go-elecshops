68729a152531777d3d1996f301429266b9d7df39
ivanbaug <ivanbgp@gmail.com>
2023-03-03 16:40:29 -0500

    fix: not null cols, this project is too small to deal with go's nulls

--- db/migrations/000001_init_tables.up.sql
+++ db/migrations/000001_init_tables.up.sql
@@ -2,25 +2,25 @@ CREATE TABLE "store"(
                         "id" BIGSERIAL NOT NULL,
                         "name" VARCHAR(255) NOT NULL,
                         "url" VARCHAR(255) NOT NULL,
-                        "country" VARCHAR(255) NULL DEFAULT 'Colombia',
-                        "region" VARCHAR(255) NULL DEFAULT 'LATAM',
-                        "bad_ping_count" BIGINT NULL DEFAULT 0
+                        "country" VARCHAR(255) NOT NULL DEFAULT 'Colombia',
+                        "region" VARCHAR(255) NOT NULL DEFAULT 'LATAM',
+                        "bad_ping_count" BIGINT NOT NULL DEFAULT 0
                     );
 ALTER TABLE
     "store" ADD PRIMARY KEY("id");
 CREATE TABLE "product"(
                           "id" BIGSERIAL NOT NULL,
                           "sku" VARCHAR(255) NOT NULL,
-                          "description" VARCHAR(255) NULL,
-                          "vendor" VARCHAR(255) NULL,
-                          "stock" BIGINT NOT NULL DEFAULT 0,
-                          "price" BIGINT NULL,
-                          "times_clicked_update" BIGINT NULL DEFAULT 0,
+                          "description" varchar(255) DEFAULT ''::character varying NOT NULL,
+                          "vendor" varchar(255) DEFAULT ''::character varying NOT NULL,
+                          "stock" BIGINT DEFAULT 0 NOT NULL,
+                          "price" BIGINT DEFAULT 0 NOT NULL,
+                          "times_clicked_update" BIGINT NOT NULL DEFAULT 0,
                           "id_store" BIGINT NOT NULL,
                           "last_update" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
                           "first_update" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
                           "num_updates" BIGINT NOT NULL DEFAULT 0,
-                          "url" VARCHAR(255) NULL
+                          "url" varchar(255) DEFAULT ''::character varying NOT NULL
                       );
 ALTER TABLE
     "product" ADD PRIMARY KEY("id");
